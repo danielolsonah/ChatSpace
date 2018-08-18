57d28a757784d222bd47ef66dfd3bbe892051276
Dan Olson <danielolsonah.school@gmail.com>
2018-08-18 13:49:05 -0700

    Floated right column to the right, fixed spacing

--- client/components/ChatRoom.jsx
+++ client/components/ChatRoom.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { sendChat } from '../actions/index.js';
+
+const mapStateToProps = (state) => {
+	return {
+		username: state.username,
+		chats: state.chats
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		sendChat: (username, message) => {
+			dispatch(sendChat(username, message))
+		}
+	}
+}
 
 class ChatRoom extends React.Component {
 	constructor(props) {
 		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick() {
+		this.props.sendChat(this.props.username, this.refs.chatInput.value)
 	}
 	render() {
 		return (
 			<div id='chatRoom'>
 			 	CHAT PLACEHOLDER
-				<div id='chatDisplay'></div>
-				<div id='chatInput'></div>
+				<div id='chatDisplay'>
+					{this.props.chats.map(chat => (
+						<div className='chatEntry'><strong>{chat.username}:</strong> {chat.message}</div>
+					))}
+				</div>
+				<div id='chatForm'>
+					<input type='text' id='chatInput' ref='chatInput' />
+					<button onClick={this.handleClick} >Send</button>
+				</div>
 			</div>
 		)
 	}
 }
 
-export default ChatRoom;
+const ChatRoomContainer = connect(mapStateToProps, mapDispatchToProps)(ChatRoom);
+
+export default ChatRoomContainer;
